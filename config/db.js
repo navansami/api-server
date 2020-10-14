ff677cb479649742de9d6b51dfbcb5043c159dcd
Navan Sami <navan.sami@yahoo.com>
2020-10-14 19:06:45 +0200

    added project files

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const config = require('config');
 
 const connectAtlas = config
 		.get('mongoURI')
+		.replace('<user>',config.get('user'))
 		.replace('<password>',config.get('pw'))
 		.replace('<dbname>',config.get('dbname'));
 
